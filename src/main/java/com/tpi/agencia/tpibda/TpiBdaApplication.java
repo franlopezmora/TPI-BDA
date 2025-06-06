303b22616b04b1e93a0af0b13d7a73873a1510d3
Francisco López Mora <96274@mecanica.frc.utn.edu.ar>
2025-06-06 17:36:40 -0300

    -Agrego el proyecto SpringBoot y la BD

---
+++ src/main/java/com/tpi/agencia/tpibda/TpiBdaApplication.java
@@ -0,0 +1,14 @@
+package com.tpi.agencia.tpibda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiBdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpiBdaApplication.class, args);
+        System.out.println("Hola mundo");
+    }
+
+}
---
+++ src/test/java/com/tpi/agencia/tpibda/TpiBdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpi.agencia.tpibda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiBdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
