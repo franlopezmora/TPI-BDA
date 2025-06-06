a81c8d62a877dbae02beb9aa60c2d1516adb1adc
Marcos Ignacio Belli <95348@sistemas.frc.utn.edu.ar>
2025-06-06 19:12:11 -0300

    cambios modelo marca vehiculo entities

--- src/main/java/com/tpi/agencia/tpibda/Entities/Marca.java
+++ src/main/java/com/tpi/agencia/tpibda/Entities/Marca.java
@@ -1,4 +1,63 @@
 package com.tpi.agencia.tpibda.Entities;
 
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "MARCA")
 public class Marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "marca", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Modelo> modelos = new ArrayList<>();
+
+    public Marca() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Modelo> getModelos() {
+        return modelos;
+    }
+
+    public void setModelos(List<Modelo> modelos) {
+        this.modelos = modelos;
+    }
+
+    public void addModelo(Modelo modelo) {
+        modelos.add(modelo);
+        modelo.setMarca(this);
+    }
+
+    public void removeModelo(Modelo modelo) {
+        modelos.remove(modelo);
+        modelo.setMarca(null);
+    }
 }
--- src/main/java/com/tpi/agencia/tpibda/Entities/Vehiculo.java
+++ src/main/java/com/tpi/agencia/tpibda/Entities/Vehiculo.java
@@ -1,4 +1,78 @@
 package com.tpi.agencia.tpibda.Entities;
 
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "VEHICULO")
 public class Vehiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String patente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_MODELO", nullable = false)
+    private Modelo modelo;
+
+    @OneToMany(mappedBy = "vehiculo", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Posicion> posiciones = new ArrayList<>();
+
+    public Vehiculo() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    public List<Posicion> getPosiciones() {
+        return posiciones;
+    }
+
+    public void setPosiciones(List<Posicion> posiciones) {
+        this.posiciones = posiciones;
+    }
+
+    public void addPosicion(Posicion posicion) {
+        posiciones.add(posicion);
+        posicion.setVehiculo(this);
+    }
+
+    public void removePosicion(Posicion posicion) {
+        posiciones.remove(posicion);
+        posicion.setVehiculo(null);
+    }
 }
