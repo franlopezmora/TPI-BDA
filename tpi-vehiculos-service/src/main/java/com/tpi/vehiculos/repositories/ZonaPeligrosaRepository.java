8b2ab497f2e5f0ee112c4b199ca4c83de9261a03
Marcos Ignacio Belli <95348@sistemas.frc.utn.edu.ar>
2025-06-09 11:23:18 -0300

    creacion de entidades del micro de vehiculo

--- tpi-vehiculos-service/src/main/java/com/tpi/vehiculos/repositories/ZonaPeligrosaRepository.java
+++ tpi-vehiculos-service/src/main/java/com/tpi/vehiculos/repositories/ZonaPeligrosaRepository.java
@@ -1,4 +1,10 @@
 package com.tpi.vehiculos.repositories;
 
-public interface ZonaPeligrosaRepository {
+
+import com.tpi.vehiculos.entities.ZonaPeligrosa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZonaPeligrosaRepository extends JpaRepository<ZonaPeligrosa, Long> {
 }
