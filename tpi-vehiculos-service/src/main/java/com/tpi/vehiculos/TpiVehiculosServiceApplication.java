35b1c266635f1352108406a77bd21fb402ae81f5
franlopezmora <franciscolopezmora3@gmail.com>
2025-06-10 19:41:13 -0300

    - Agrego logs en los sv.

--- tpi-vehiculos-service/src/main/java/com/tpi/vehiculos/TpiVehiculosServiceApplication.java
+++ tpi-vehiculos-service/src/main/java/com/tpi/vehiculos/TpiVehiculosServiceApplication.java
@@ -2,12 +2,19 @@ package com.tpi.vehiculos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TpiVehiculosServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpiVehiculosServiceApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void logStartup() {
+		System.out.println("\n 🚗 Microservicio VEHICULOS corriendo en el puerto 8082 \n");
 	}
 }
 
