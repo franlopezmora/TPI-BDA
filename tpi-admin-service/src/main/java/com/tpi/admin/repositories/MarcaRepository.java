f671c3e40974952d056bd5e95cfc2fe0d19ffe6b
nicolas <nicolassotogaray@gmail.com>
2025-06-13 19:54:20 -0300

    Entidades, controllers, repositories y services agregados de más movidos a carpeta backup dentro de admin.

--- tpi-admin-service/src/main/java/com/tpi/admin/repositories/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tpi.admin.repositories;
-
-import com.tpi.admin.entities.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends JpaRepository<Marca, Integer> {
-}
