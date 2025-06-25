6a62529b3a93e1791a66ecf2393d4bd6ff2804a9
franlopezmora <franciscolopezmora3@gmail.com>
2025-06-25 18:27:53 -0300

    - Elimino carpeta de backup.

--- tpi-admin-service/src/main/java/com/tpi/admin/backup/repositories/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tpi.admin.backup.repositories;
-
-import com.tpi.admin.backup.entities.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends JpaRepository<Marca, Integer> {
-}
--- tpi-admin-service/src/main/java/com/tpi/admin/backup/repositories/ZonaPeligrosaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tpi.admin.backup.repositories;
-
-import com.tpi.admin.backup.entities.ZonaPeligrosa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZonaPeligrosaRepository extends JpaRepository<ZonaPeligrosa, Integer> {
-}
